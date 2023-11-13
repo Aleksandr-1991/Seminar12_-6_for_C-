d21b90a37a932e75ff94782b0bd8561613a3b55e
Aleks37 <lao35180972643335589007>
2023-11-13 23:04:03 +0400

    Commit 1 from 13.11.23

---
+++ Task4_(in_groups)/Program.cs
@@ -0,0 +1,13 @@
+﻿//Зад. 44. Нап команду для создания послед-ти Фибоначчи.
+int[] Febanachi (int N)
+{
+    int[] array = new int[N];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i =2; i < N; i++)
+    {
+        array[i] = array[i-1] + array[i-2];
+    }
+    return array;
+}
+Console.Write(string.Join(" ", Febanachi(8)));
